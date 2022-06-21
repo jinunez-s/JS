2ecb67c0ab1803d7cafc167990dbd9bfbdbca440
Julio Nuñez <julionr14@gmail.com>
2022-06-21 16:55:39 -0600

    stat

--- sql/intro.sql
+++ sql/intro.sql
@@ -1006,4 +1006,8 @@ CREATE TABLE follows(
     PRIMARY KEY(follower_id, followee_id)
 );
 
-INSERT INTO follows
+INSERT INTO follows(follower_id, followee_id) VALUES
+(1,2),
+(1,3),
+(3,1),
+(2,3);
