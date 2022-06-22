05023ff8913e215baa4d3447f52113df267caabe
Julio Nuñez <julionr14@gmail.com>
2022-06-22 13:50:08 -0600

    IG photo

--- sql/intro.sql
+++ sql/intro.sql
@@ -1011,3 +1011,19 @@ INSERT INTO follows(follower_id, followee_id) VALUES
 (1,3),
 (3,1),
 (2,3);
+
+
+/*indetify most popular photo*/
+SELECT 
+    username,
+    photos.id,
+    photos.image_url, 
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
