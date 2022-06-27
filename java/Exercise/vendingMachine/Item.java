c4dcab0ffcf134cfcbb9624f0faf8b645325b480
Julio Nuñez <julionr14@gmail.com>
2022-06-26 22:03:33 -0600

    Item update

---
+++ java/Exercise/vendingMachine/Item.java
@@ -0,0 +1,19 @@
+public class Item {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Item(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Item(Item source){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    
+}
