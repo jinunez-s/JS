19cc02a50478f405d74a411e746b965444b39a9d
Julio Nuñez <julionr14@gmail.com>
2022-06-26 22:14:49 -0600

    Update main

--- java/Exercise/vendingMachine/VendingMachine.java
+++ java/Exercise/vendingMachine/VendingMachine.java
@@ -1,5 +1,9 @@
 public class VendingMachine{
     public static void main(String[] args) {
-        
+        Item[][] items = new Item[][] {
+            {new Item("Pepsi", 4.95, 10), new Item("Fresca", 4.95, 5), new Item("Fanta", 4.95, 10)},
+            {new Item("Coke", 4.95, 10), new Item("DrPepper", 6.00, 10), new Item("Sprite", 4.95, 15)},
+            {new Item("7-Up", 4.95, 15), new Item("Pepsi black", 4.95, 10), new Item("Uva", 4.95, 15)}
+        };
     }
 }
