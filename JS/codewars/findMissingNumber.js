b5f3f30718f7af5664f24a0bb03eda1efbb0c186
Julio Nuñez <julionr14@gmail.com>
2022-07-10 23:29:56 -0600

    SumArrays

--- JS/codewars/findMissingNumber.js
+++ JS/codewars/findMissingNumber.js
@@ -1,15 +1,30 @@
+function findMissingNumbers(arr){
+    var firstNumber = arr[0];
+    var arrTwo = [];
+    var cont = 0;
+        for(let i = 0; i < arr.length; i++){
+            do{
+                if(arr[i] != (firstNumber + cont) ){
+                arrTwo.push(firstNumber + cont);
+            }
+            cont++;
+            }
+            while((firstNumber+ cont) <= arr[i]);
+            
+        };
+        // for (let index = 0; index < arrTwo.length; index++) {
+        //     console.log(arrTwo[index]);        
+        // }
+    return arrTwo;
+}
 var arr = [-3, -2, 1 , 4];
-var firstNumber = arr[0];
-var aux = 0;
-var lastNumber = arr[arr.length - 1];
-var arrTwo = [];
-var cont = 0;
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] != (firstNumber + cont) ){
-            arrTwo.push(firstNumber + cont);
-        }
-        cont++;
-    };
-    for (let index = 0; index < arrTwo.length; index++) {
-        console.log(arrTwo[index]);        
+//console.log(findMissingNumbers(arr));
+
+function findMissingNumber2(arr){
+    var output = [];
+    for(let i = arr[0]; i < arr[arr.length - 1]; i++){
+        if(arr.indexOf(i) === -1){output.push(i)}
     }
+    return output;
+}
+console.log(findMissingNumber2(arr));
---
+++ JS/codewars/sumArrays.js
@@ -0,0 +1,9 @@
+var sum = 0;
+var numbers = [-1, -2, -3, -2.2, -2.2254];
+for(let i = 0; i < numbers.length; i++){
+    sum += numbers[i];
+}
+console.log(sum);
+//reduce function
+const suma = numbers.reduce((a,b) => a + b, sum);
+console.log(suma);
