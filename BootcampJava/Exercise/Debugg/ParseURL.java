c66fdc190e45d8f13fc51abd7f0ec5185fdf07a7
Julio Nuñez <julionr14@gmail.com>
2022-08-03 21:58:12 -0600

    Parse

---
+++ BootcampJava/Exercise/Debugg/ParseURL.java
@@ -0,0 +1,10 @@
+import java.net.URL;
+
+public class ParseURL{
+    public static void main(String []args){
+        try{
+            URL url = new URL("https://www.tradingview.com/chart/KfqcJbuv/?symbol=BITSTAMP%3ABTCUSD");
+            
+        }
+    }
+}
